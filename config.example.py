f9d8d774bd39bcb0b5a72b988d75957a5230cd9e
OhhLenzo <foster.brendan01@gmail.com>
2026-01-25 18:58:22 -1000

    Add example config file

---
+++ config.example.py
@@ -0,0 +1,7 @@
+# Rename this file to config.py and add your own values
+
+DISCORD_WEBHOOK_URL= "https://discord.com/api/webhooks/1465208383668752392/IggFXohd8r-rFbkvAmXZ4k3vu8L-YuwK3-gbPZ3mxcWlQChBwCg2QZbO7NtQtkWsfM_z"
+
+PRODUCT_URL = "https://www.pokemoncenter.com/category/elite-trainer-box?category=tcg-cards"
+
+CHECK_INTERVAL = 60 # seconds
