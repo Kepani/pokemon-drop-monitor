02723ed5c986164929b4ab210a95c85ed121d85f
OhhLenzo <foster.brendan01@gmail.com>
2026-01-27 15:01:26 -1000

    removed pre defined url

--- config.py
+++ config.py
@@ -1,5 +1,3 @@
-DISCORD_WEBHOOK_URL= "https://discord.com/api/webhooks/1465208383668752392/IggFXohd8r-rFbkvAmXZ4k3vu8L-YuwK3-gbPZ3mxcWlQChBwCg2QZbO7NtQtkWsfM_z"
-
-PRODUCT_URL = "https://www.pokemoncenter.com/category/elite-trainer-box?category=tcg-cards"
+DISCORD_WEBHOOK_URL = "https://discord.com/api/webhooks/1465208383668752392/IggFXohd8r-rFbkvAmXZ4k3vu8L-YuwK3-gbPZ3mxcWlQChBwCg2QZbO7NtQtkWsfM_z"
 
 CHECK_INTERVAL = 60 # seconds
